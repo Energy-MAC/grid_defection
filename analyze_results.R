4f0562e8321fec5793b8fb3b0787292e1dd36abd
Will Gorman <will.gorman5@gmail.com>
2018-11-23 17:08:23 -0800

    more results updates

--- analyze_results.R
+++ analyze_results.R
@@ -34,10 +34,8 @@ rm(base_rates_100,base_rates_95)
 sizing_1 <- fread(paste0(DIR,OUT,"\\600pv_100stor (min const).csv"))
 sizing_1$version <- "600pv_100stor"
 
-sizing_2 <- fread(paste0(DIR,OUT,"\\1200pv_400stor.csv"))
+sizing_2 <- fread(paste0(DIR,OUT,"\\1200pv_400stor (min const).csv"))
 sizing_2$version <- "1200pv_400stor"
-
-
 
 #fip codes
 fips <- fread(paste0(DIR,OUT,"\\fips.csv"))
@@ -46,7 +44,7 @@ fips <- fread(paste0(DIR,OUT,"\\fips.csv"))
 ## II. Simple graphs #####################################
 ##########################################################
 #select analysis sample
-sizing <- sizing_1
+sizing <- rbind(sizing_1,sizing_2)
 
 ##plotting reliability of 0
 ggplot(data=sizing[reliability=="0"], aes(pv,storage)) + geom_point(aes(color=case)) +
@@ -70,28 +68,12 @@ ggplot(data=sizing[reliability=="0.05"], aes(pv,storage)) + geom_point(aes(color
 
 ggsave(filename = paste0(DIR,OUT, "\\images\\sizing_reliability_0.05.jpg"))
 
-##ratio analysis
-sizing$ratio <- sizing$storage/sizing$pv
-test <- reshape(sizing[,c(2,3,5:9)], idvar = c("case","county","state"), 
-                timevar="reliability", v.names=c("ratio","pv","storage"), direction="wide")
-test$ratio_diff <- test$ratio.0.05 - test$ratio.0
-
-##plot ratios
-ggplot(test, aes(x=case, y=ratio_diff)) + 
-  geom_boxplot() + xlab(label = "Load Case") + ylab(label = "Solar size (kW)") + 
-  theme(axis.text=element_text(size=18),axis.title=element_text(size=20,face="bold"), 
-        legend.text=element_text(size=20),legend.title=element_text(size=20,face="bold"),
-        legend.position = c(0.7,.8)) + 
-  scale_fill_manual(values = c("cadetblue4", "darkgoldenrod3"),labels = c("100%", "95%")) +
-  guides(colour = guide_legend(override.aes = list(size=10))) #+ 
-  #scale_y_continuous(breaks=seq(0,400,50), limits=c(0,400))
-
 ##box and whisker (solar)
 ggplot(sizing, aes(x=case, y=pv, fill=reliability)) + 
   geom_boxplot() + xlab(label = "Load Case") + ylab(label = "Solar size (kW)") + 
   theme(axis.text=element_text(size=18),axis.title=element_text(size=20,face="bold"), 
         legend.text=element_text(size=20),legend.title=element_text(size=20,face="bold"),
-        legend.position = c(0.7,.8)) + 
+        legend.position = c(0.9,.8)) + 
   scale_fill_manual(values = c("cadetblue4", "darkgoldenrod3"),labels = c("100%", "95%")) +
   guides(colour = guide_legend(override.aes = list(size=10))) + 
   scale_y_continuous(breaks=seq(0,300,50), limits=c(0,300))
@@ -103,13 +85,12 @@ ggplot(sizing, aes(x=case, y=storage, fill=reliability)) +
   geom_boxplot() + xlab(label = "Load Case") + ylab(label = "Storage size (kW)") + 
   theme(axis.text=element_text(size=18),axis.title=element_text(size=20,face="bold"), 
         legend.text=element_text(size=20),legend.title=element_text(size=20,face="bold"),
-        legend.position = c(0.7,.8)) +  
+        legend.position = c(0.9,.8)) +  
   scale_fill_manual(values = c("cadetblue4", "darkgoldenrod3"),labels = c("100%", "95%")) +
   guides(colour = guide_legend(override.aes = list(size=10)))+ 
   scale_y_continuous(breaks=seq(0,700,100), limits=c(0,700)) 
 
 ggsave(filename = paste0(DIR,OUT, "\\images\\storage_sizing.jpg"))
-
 
 ##comparing reliability difference
 merged <- merge(sizing_3[,c(2,3,5:8)], sizing_4[,c(2,3,5:8)], by =c("reliability","case","county","state"))
@@ -147,7 +128,29 @@ l_stats <- sizing %>% group_by(case) %>% summarize(max_pv = max(pv),
 
 ##average stats (by reliability)
 r_stats <- sizing %>% group_by(reliability) %>% summarize(mean_sol = mean(pv),
-                                                   mean_pv = mean(storage))
+                                                   mean_pv = mean(storage),
+                                                   max_pv = max(pv),
+                                                   min_pv = min(pv),
+                                                   med_pv = median(pv),
+                                                   max_stor = max(storage),
+                                                   min_stor = min(storage),
+                                                   med_stor = median(storage))
+
+##ratio analysis
+sizing$ratio <- sizing$storage/sizing$pv
+test <- reshape(sizing[,c(2,3,5:9)], idvar = c("case","county","state"), 
+                timevar="reliability", v.names=c("ratio","pv","storage"), direction="wide")
+test$ratio_diff <- test$ratio.0.05 - test$ratio.0
+
+##plot ratios
+ggplot(test, aes(x=case, y=ratio_diff)) + 
+  geom_boxplot() + xlab(label = "Load Case") + ylab(label = "Solar size (kW)") + 
+  theme(axis.text=element_text(size=18),axis.title=element_text(size=20,face="bold"), 
+        legend.text=element_text(size=20),legend.title=element_text(size=20,face="bold"),
+        legend.position = c(0.7,.8)) + 
+  scale_fill_manual(values = c("cadetblue4", "darkgoldenrod3"),labels = c("100%", "95%")) +
+  guides(colour = guide_legend(override.aes = list(size=10))) #+ 
+#scale_y_continuous(breaks=seq(0,400,50), limits=c(0,400))
 
 
 ##########################################################
@@ -323,6 +326,28 @@ for (index in 1:length(rate)){
 ##########################################################
 ## III. Sizes vs. cost assumptions #######################
 ##########################################################
+#get costs
+#Set costs
+BAT_COST = 100 # $/kWh
+PV_COST = 600 # $/kW
+LOAD_COST = 800 # $/kW peak load
+OM_COST = 100 # $/kW peak load per year
+
+sizing_1 <- merge(sizing_1,opt_long,by=c("county","state","case"))
+sizing_1$cost <- sizing_1$pv * PV_COST * PV_RATE + sizing_1$storage * BAT_COST * BAT_RATE +
+  sizing_1$max_load * LOAD_COST * INVT_RATE + OM_COST * sizing_1$max_load
+
+#get costs
+#Set costs
+BAT_COST = 400 # $/kWh
+PV_COST = 1200 # $/kW
+LOAD_COST = 1600 # $/kW peak load
+OM_COST = 100 # $/kW peak load per year
+
+sizing_2 <- merge(sizing_2,opt_long,by=c("county","state","case"))
+sizing_2$cost <- sizing_2$pv * PV_COST * PV_RATE + sizing_2$storage * BAT_COST * BAT_RATE +
+  sizing_2$max_load * LOAD_COST * INVT_RATE + OM_COST * sizing_2$max_load
+
 compiled <- rbind(sizing_1, sizing_2)
 
 ##average stats
